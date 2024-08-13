7a8c728d68a6f9eb983f51aeaaad371ed44675d6
Harut <zaqaryanharut242gmail.com>
2024-08-13 20:28:34 +0400

    Complete

--- task08/app/lambdas/api_handler/index.js
+++ task08/app/lambdas/api_handler/index.js
@@ -1,36 +1,4 @@
-// import OpenMeteoAPI from "../layers/api_handler_layer/openMateoAPI";
 import axios from "axios";
-// class OpenMeteoAPI {
-//   constructor(latitude, longitude) {
-//     this.baseUrl =
-//       "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
-//     this.latitude = latitude;
-//     this.longitude = longitude;
-
-//     console.log("~~~latitude(from lambda layer)~~~", this.latitude);
-//     console.log("~~~longitude(from lambda layer)~~~", this.longitude);
-//   }
-
-//   async getWeatherForecast() {
-//     try {
-//       const response = await axios.get(this.baseUrl, {
-//         params: {
-//           latitude: this.latitude,
-//           longitude: this.longitude,
-//           current: "temperature_2m,wind_speed_10m",
-//           hourly: "temperature_2m,relative_humidity_2m,wind_speed_10m",
-//         },
-//       });
-//       console.log("~~~RESPONSE(from lambda LAYER)~~~", response);
-
-//       return response.data;
-//     } catch (error) {
-//       console.error("~~~Error from layer's try", error);
-//       throw error;
-//     }
-//   }
-// }
-
 class OpenMeteoAPI {
   constructor() {
     this.baseUrl = "https://api.open-meteo.com/v1/forecast";
@@ -55,22 +23,13 @@ class OpenMeteoAPI {
 }
 
 export const handler = async (event) => {
-  console.log("~~~EVENT(from lambda func)~~~", event);
-
-  //   const latitude = event.queryStringParameters.latitude || "52.52";
-  //   console.log("~~~latitude(from lambda func)~~~", latitude);
-  //   const longitude = event.queryStringParameters.longitude || "13.41";
-  //   console.log("~~~longitude(from lambda func)~~~", longitude);
-  
   const latitude = 52.52;
   const longitude = 13.41;
 
   const api = new OpenMeteoAPI();
-  console.log("~~~API Class instance(from lambda func)~~~", api);
 
   try {
-    const forecast = await api.getWeatherForecast(latitude,longitude);
-    console.log("~~~forecast(from lambda func)~~~", event);
+    const forecast = await api.getWeatherForecast(latitude, longitude);
     return {
       statusCode: 200,
       body: JSON.stringify(forecast),
